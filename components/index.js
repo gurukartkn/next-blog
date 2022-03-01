5fdbf5b5cc067f3eb20f00780926c8f247d84956
Guru Karthikeyan <gurukartkn@gmail.com>
2022-03-01 20:15:14 +0530

    layout commit

--- components/index.js
+++ components/index.js
@@ -4,3 +4,7 @@ export { default as Categories }
 from './Categories'
 export { default as PostWidget }
 from './PostWidget'
+export { default as Header }
+from './Header'
+export { default as Layout }
+from './Layout'
