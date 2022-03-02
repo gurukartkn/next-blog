828c475acc0b14aaa9e37c40920b8c88fc45d2b3
Guru Karthikeyan <gurukartkn@gmail.com>
2022-03-02 19:46:39 +0530

    post page commit

--- services/index.js
+++ services/index.js
@@ -2,8 +2,8 @@ import { request, gql } from "graphql-request";
 
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
-export const getPosts = async() => {
-    const query = gql `
+export const getPosts = async () => {
+  const query = gql`
     query MyQuery {
       postsConnection {
         edges {
@@ -33,13 +33,48 @@ export const getPosts = async() => {
     }
   `;
 
-    const result = await request(graphqlAPI, query);
+  const result = await request(graphqlAPI, query);
 
-    return result.postsConnection.edges;
+  return result.postsConnection.edges;
 };
 
-export const getRecentPosts = async() => {
-    const query = gql `
+export const getPostDetails = async (slug) => {
+  const query = gql`
+    query GetPostDetails($slug: String!) {
+      post(where: { slug: $slug }) {
+        author {
+          bio
+          name
+          id
+          photo {
+            url
+          }
+        }
+        createdAt
+        slug
+        title
+        excerpt
+        featuredImage {
+          url
+        }
+        categories {
+          name
+          slug
+        }
+        content {
+          raw
+        }
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query, { slug });
+
+  return result.post;
+};
+
+export const getRecentPosts = async () => {
+  const query = gql`
     query GetPostDetails {
       posts(orderBy: createdAt_ASC, last: 3) {
         title
@@ -52,13 +87,13 @@ export const getRecentPosts = async() => {
     }
   `;
 
-    const result = await request(graphqlAPI, query);
+  const result = await request(graphqlAPI, query);
 
-    return result.posts;
+  return result.posts;
 };
 
-export const getSimilarPosts = async() => {
-    const query = gql `
+export const getSimilarPosts = async (categories, slug) => {
+  const query = gql`
     query GetPostDetails($slug: String!, $categories: [String!]) {
       posts(
         where: {
@@ -76,22 +111,22 @@ export const getSimilarPosts = async() => {
       }
     }
   `;
-    const result = await request(graphqlAPI, query);
+  const result = await request(graphqlAPI, query, { categories, slug });
 
-    return result.posts;
+  return result.posts;
 };
 
-export const getCategories = async() => {
-    const query = gql `
+export const getCategories = async () => {
+  const query = gql`
     query GetGategories {
-        categories {
-          name
-          slug
-        }
+      categories {
+        name
+        slug
+      }
     }
   `;
 
-    const result = await request(graphqlAPI, query);
+  const result = await request(graphqlAPI, query);
 
-    return result.categories;
+  return result.categories;
 };
